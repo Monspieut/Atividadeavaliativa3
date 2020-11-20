34479cf452b2c52cb83c703e381644db31558a32
Alex Alexandre Alves <alex.alves@alunos.sc.senac.br>
2020-11-19 21:41:12 -0300

    Update

--- Locacao.java
+++ Locacao.java
@@ -104,7 +104,6 @@ public class Locacao {
         calendario.setTime(getDtdevolucao());
         calendario.add(Calendar.DATE, qtddedias);
         return calendario.getTime();
-
     }
 
 }
--- Menu.java
+++ Menu.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.util.Date;
+import java.util.Calendar;
 
 public class Menu {
     public static void main(String[] args) {
 
         Scanner menu = new Scanner(System.in);
         while (true) {
-            System.out.println("\n\n Sistema de Locação de Veiculos ###");
+            System.out.println("\n\n            Sistema de Locação de Veiculos ");
             System.out.println("\n                 =========================");
             System.out.println("                      1 - Cadastrar Cliente         ");
             System.out.println("                      2 - Cadastrar Veículo     ");
@@ -14,14 +16,38 @@ public class Menu {
             System.out.println("                      5 - Listar Veículos    ");
             System.out.println("                      6 - Listar Locações   ");
             System.out.println("                      0 - Sair          ");
-            System.out.println("                  =========================\n");
+            System.out.println("                   ========================\n");
 
             int opcao = menu.nextInt();
             switch (opcao) {
                 case 1:
                     System.out.println(" CADASTRAR CLIENTE: ");
-                    
+                    System.out.println("Digite o ID do cliente: ");
+                    int id = scanner.nextInt();
+                    System.out.println("Digite o nome do cliente: ");
+                    String nome = scanner.nextLine();
+                    System.out.println("Digite o C.P.F. do cliente: ");
+                    String cpf = scanner.nextLine();
+
+                    boolean dateOK = false;
+                    Date dtnascimento = new Date();
+                    do {
+                        System.out.println("Digite a data de nascimento do cliente (dd/mm/aaaa): ");
+                        String stringDate = scanner.nextLine();
+                        try {
+                            dtnascimento = UseCalendar.parseDate(stringDate);
+                            dateOK = true;
+                        } catch (Exception e) {
+                            System.out.println("Data em formato inválido, digite novamente.");
+                        }
+                    } while (!dateOK);
+
+                    System.out.println("Digite os dias de devolução do cliente: ");
+                    int diasparadevolucao = scanner.nextInt();
+                    new Cliente(id, nome, dtnascimento, cpf, diasparadevolucao);
+
                     break;
+
                 case 2:
 
                     System.out.println(" CADASTRAR VEICULO: ");
